966493e47646b1befac7fc75121801dafed0398a
leov-hg <lv@homagames.com>
2022-01-21 22:48:18 +0100

    WIP PlayerBump

--- Assets/Scripts/PlayerBump.cs
+++ Assets/Scripts/PlayerBump.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerBump : PlayerBase
 {
+    [SerializeField] private float ejectForce;
+    [SerializeField] private float ejectionRadius;
+
+    private List<Rigidbody> _detectedObjects;
+    private List<Collider> _detectedCollider;
+
     protected override void Interact()
     {
-        base.Interact();
+        ScanObjectsAround();
+
+        foreach (Rigidbody obj in _detectedObjects)
+        {
+            obj.AddForce((obj.transform.position - transform.position).normalized * ejectForce);
+        }
+    }
+
+    private void ScanObjectsAround()
+    {
+        _detectedCollider = Physics.OverlapSphere(transform.position, ejectionRadius).ToList();
+        foreach (Collider col in _detectedCollider)
+        {
+            _detectedObjects.Add(col.GetComponent<Rigidbody>());
+        }
     }
 }
