a2d5fed9162ec7e82268085f49591126071ce507
Liam Lepinay <lil@homagames.com>
2022-01-21 22:48:24 +0100

    UPDATE

--- Assets/Scripts/PlayerBase.cs
+++ Assets/Scripts/PlayerBase.cs
@@ -1,3 +1,4 @@
+using MyBox;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,9 +6,23 @@ using UnityEngine;
 
 public class PlayerBase : MonoBehaviour
 {
+    [Separator("References", true)]
+    [SerializeField] private Rigidbody rb;
+
+    [Separator("Settings", true)]
+    [SerializeField] private float speed = 1;
+
+    private Vector3 _direction;
+
+
     private void Update()
     {
-        //todo : movement
+        _direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(_direction, ForceMode.Acceleration);
     }
 
     virtual protected void Interact()
