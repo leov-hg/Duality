86a77beef36d1913f3e7de5d2665381b18ea761e
leov-hg <lv@homagames.com>
2022-01-22 17:41:27 +0100

    enemy knock back animation

--- Assets/Scripts/PhysicsHandler.cs
+++ Assets/Scripts/PhysicsHandler.cs
@@ -1,10 +1,12 @@
 using System;
+using MyBox;
 using UnityEngine;
 
 public class PhysicsHandler : MonoBehaviour
 {
     public float bumpWeight = 1;
     public float vacuumWeight = 1;
+    public float bumpUpliftModifier;
 
     private Rigidbody _rb;
     public Rigidbody Rb => _rb;
@@ -16,7 +18,7 @@ public class PhysicsHandler : MonoBehaviour
 
     public void ApplyBumpForce(Vector3 forceDirection, float ejectForce)
     {
-        _rb.AddForce(forceDirection.normalized * (ejectForce / bumpWeight), ForceMode.VelocityChange);
+        _rb.AddForce(forceDirection.SetY(bumpUpliftModifier).normalized * (ejectForce / bumpWeight), ForceMode.VelocityChange);
     }
 
     public void ApplyVacuumForce(Vector3 forceDirection, float vacuumforce)
